926a2421025498882ca04df82eced66d2be33867
Alan Obregón <alan.obregon49@gmail.com>
2021-09-02 11:20:35 -0300

    MERCH-0001 - added ReponseBody annotation

--- src/main/java/com/informatorio/infomerchandising/controllers/CityController.java
+++ src/main/java/com/informatorio/infomerchandising/controllers/CityController.java
@@ -3,7 +3,6 @@ package com.informatorio.infomerchandising.controllers;
 import com.informatorio.infomerchandising.dtos.CityRequest;
 import com.informatorio.infomerchandising.services.CityService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +20,7 @@ public class CityController {
 	}
 
 	@GetMapping
+	@ResponseBody
 	public ResponseEntity<?> findAll(@RequestParam(required = false) String name) {
 		return (name != null) ?
 			cityService.findAllByNameContaining(name)
@@ -28,21 +28,25 @@ public class CityController {
 	}
 
 	@PostMapping
+	@ResponseBody
 	public ResponseEntity<?> save(@Valid @RequestBody CityRequest request) {
 		return cityService.save(request);
 	}
 
 	@GetMapping(value = "/{id}")
+	@ResponseBody
 	public ResponseEntity<?> findById(@PathVariable("id") Long id) {
 		return cityService.findById(id);
 	}
 
 	@PutMapping(value = "/{id}")
+	@ResponseBody
 	public ResponseEntity<?> updateById(@PathVariable("id") Long id, @RequestBody CityRequest request) {
 		return cityService.updateById(id, request);
 	}
 
 	@DeleteMapping(value = "/{id}")
+	@ResponseBody
 	public ResponseEntity<?> deleteById(@PathVariable("id") Long id) {
 		return cityService.deleteById(id);
 	}
