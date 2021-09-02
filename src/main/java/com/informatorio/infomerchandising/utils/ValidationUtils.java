4fd41cb0d6152f199e2e6130c448377b06b93f12
Alan Obregón <alan.obregon49@gmail.com>
2021-09-02 12:52:14 -0300

    MERCH-0004 - added email format validation

--- src/main/java/com/informatorio/infomerchandising/utils/ValidationUtils.java
+++ src/main/java/com/informatorio/infomerchandising/utils/ValidationUtils.java
@@ -1,6 +1,11 @@
 package com.informatorio.infomerchandising.utils;
 
-public class ValidationUtils {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public abstract class ValidationUtils {
+
+	private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
 	public static Boolean positiveNumber(Double number) {
 		return number > 0;
@@ -12,5 +17,10 @@ public class ValidationUtils {
 
 	public static Boolean stringLengthValidation(String str, int min, int max) {
 		return str.length() >= min && str.length() <= max && !str.isBlank();
+	}
+
+	public static Boolean emailValidation(String str) {
+		Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(str);
+		return matcher.find();
 	}
 }
